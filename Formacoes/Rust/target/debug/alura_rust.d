532d5792be4047be73d8ad65ae151d0660a12dbb
cristoffer_pogan <cristoffer_pogan@econeteditora.com.br>
2025-04-11 17:18:47 -0300

    feat/intro to Rust

---
+++ Formacoes/Rust/target/debug/alura_rust.d
@@ -0,0 +1,1 @@
+/home/cristoffer_pogan/Entwicklung/Alura/Formacoes/Rust/target/debug/alura_rust: /home/cristoffer_pogan/Entwicklung/Alura/Formacoes/Rust/src/main.rs
---
+++ Formacoes/Rust/target/debug/deps/alura_rust-3673864728d273b9.d
@@ -0,0 +1,5 @@
+/home/cristoffer_pogan/Entwicklung/Alura/Formacoes/Rust/target/debug/deps/libalura_rust-3673864728d273b9.rmeta: src/main.rs
+
+/home/cristoffer_pogan/Entwicklung/Alura/Formacoes/Rust/target/debug/deps/alura_rust-3673864728d273b9.d: src/main.rs
+
+src/main.rs:
