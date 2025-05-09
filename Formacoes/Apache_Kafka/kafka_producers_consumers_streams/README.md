446e6116a6921ba9f7a8e17606801232062c4750
cristoffer_pogan <cristoffer_pogan@econeteditora.com.br>
2025-05-09 17:20:03 -0300

    01. Producers, and consumers

---
+++ Formacoes/Apache_Kafka/kafka_producers_consumers_streams/README.md
@@ -0,0 +1,1 @@
+[Kafka: prdutores, consumidores e streams](https://cursos.alura.com.br/course/kafka-introducao-a-streams-em-microservicos)
---
+++ Formacoes/Apache_Kafka/kafka_producers_consumers_streams/kafka_consumer.py
@@ -0,0 +1,23 @@
+from kafka import KafkaConsumer
+import json
+
+
+def main() -> None:
+    consumer = KafkaConsumer(
+        'ECOMMERCE_NEW_ORDER',
+        bootstrap_servers='localhost:9092',
+        auto_offset_reset='earliest',
+        enable_auto_commit=True,
+        group_id='ECOMMERCE',
+        value_deserializer=lambda v: json.loads(v.decode('utf-8'))
+    )
+
+    try:
+        for message in consumer:
+            print(f"Received message: {message.value}")
+    except KeyboardInterrupt:
+        pass
+
+
+if __name__ == '__main__':
+    main()
