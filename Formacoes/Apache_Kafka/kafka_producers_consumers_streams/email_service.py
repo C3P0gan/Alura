fd9a1c942a7edc6fdf13b35fae76c895d4c2d02a
cristoffer_pogan <cristoffer_pogan@econeteditora.com.br>
2025-05-12 16:38:30 -0300

    feat/added email_service.py

---
+++ Formacoes/Apache_Kafka/kafka_producers_consumers_streams/email_service.py
@@ -0,0 +1,24 @@
+# from kafka.consumer.fetcher import ConsumerRecord
+from kafka_service import KafkaService
+
+
+class EmailService(KafkaService):
+    def __init__(self) -> None:
+        super().__init__(
+            group_id=EmailService.__class__.__name__,
+            topic='ECOMMERCE_SEND_EMAIL',
+            callback=self.parse
+        )
+
+    def parse(self, record) -> None:
+        print('-' * 60)
+        print(f"LOG: {record.topic}\n"
+              f"Key: {record.key}\n"
+              f"Value: {record.value}\n"
+              f"Partition: {record.partition}\n"
+              f"Offset: {record.offset}")
+
+
+if __name__ == '__main__':
+    with EmailService() as service:
+        service.run()
