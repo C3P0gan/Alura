d02e99d1fd0545bffd57eb45723b628a8f2194b5
cristoffer_pogan <cristoffer_pogan@econeteditora.com.br>
2025-05-13 13:35:24 -0300

    refactor/custom serialization

--- Formacoes/Apache_Kafka/kafka_producers_consumers_streams/kafka_service.py
+++ Formacoes/Apache_Kafka/kafka_producers_consumers_streams/kafka_service.py
@@ -1,23 +1,39 @@
+from typing import Self, Any, Optional
 from collections.abc import Callable
 from kafka import KafkaConsumer
-from typing import Self
 from uuid import uuid4
 
 
 class KafkaService:
-    def __init__(self, group_id: str, topic: str, callback: Callable) -> None:
+    def __init__(
+        self,
+        group_id: str,
+        topic: str,
+        callback: Callable,
+        override_properties: Optional[dict[str, Any]] = {}
+    ) -> None:
         self.callback = callback
         self.consumer = KafkaConsumer(
-            topic,
-            bootstrap_servers='127.0.0.1:9092',
-            key_deserializer=lambda k: k.decode('utf-8') if k else None,
-            value_deserializer=lambda v: v.decode('utf-8'),
-            group_id=group_id,
-            client_id=str(uuid4()),
-            auto_offset_reset='latest'
-        )
+            **self._get_properties(group_id, override_properties))
+        self.consumer.subscribe(pattern=topic)
 
-    def run(self):
+    def _get_properties(
+        self,
+        group_id: str,
+        override_properties: dict[str, Any]
+    ) -> dict[str, Any]:
+        properties = {
+            'bootstrap_servers': '127.0.0.1:9092',
+            'key_deserializer': lambda k: k.decode('utf-8') if k else None,
+            'value_deserializer': lambda v: v.decode('utf-8'),
+            'group_id': group_id,
+            'client_id': str(uuid4()),
+            'auto_offset_reset': 'earliest'  # latest
+        }
+        properties.update(override_properties)
+        return properties
+
+    def run(self) -> None:
         try:
             while True:
                 records = self.consumer.poll(timeout_ms=100)
@@ -29,7 +45,7 @@ class KafkaService:
         except KeyboardInterrupt:
             pass
 
-    def close(self):
+    def close(self) -> None:
         print('Closing consumer...')
         self.consumer.close()
 
