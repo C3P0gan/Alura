812bd1b3f621aead0b0661d61cb1208e480423e8
cristoffer_pogan <cristoffer_pogan@econeteditora.com.br>
2025-05-13 13:35:39 -0300

    refactor/custom serialization

--- Formacoes/Apache_Kafka/kafka_producers_consumers_streams/log_service.py
+++ Formacoes/Apache_Kafka/kafka_producers_consumers_streams/log_service.py
@@ -1,35 +1,22 @@
-from kafka import KafkaConsumer
+from kafka_service import KafkaService
 
 
-class LogService:
+class LogService(KafkaService):
     def __init__(self) -> None:
-        self.consumer = KafkaConsumer(
-            bootstrap_servers='localhost:9092',
-            auto_offset_reset='earliest',
-            enable_auto_commit=True,
-            group_id='log_service',
-            value_deserializer=lambda v: v.decode('utf-8')
+        super().__init__(
+            group_id=LogService.__class__.__name__,
+            topic=r'ECOMMERCE.*',
+            callback=self.parse
         )
 
-    def run(self) -> None:
-        consumer = self.consumer
-        consumer.subscribe(pattern=r'ECOMMERCE.*')
-
-        try:
-            while True:
-                records = self.consumer.poll(timeout_ms=100)
-                for topic_partition, messages in records.items():
-                    if messages:
-                        print(f"Found {len(messages)} records")
-                        for message in messages:
-                            print('-' * 60)
-                            print(f"LOG: {message.topic}\n"
-                                  f"key: {message.key}\n"
-                                  f"value: {message.value}\n"
-                                  f"offset: {message.offset}")
-        except KeyboardInterrupt:
-            pass
+    def parse(self, record) -> None:
+        print('-' * 60)
+        print(f"LOG: {record.topic}\n"
+              f"key: {record.key}\n"
+              f"value: {record.value}\n"
+              f"offset: {record.offset}")
 
 
 if __name__ == '__main__':
-    LogService().run()
+    with LogService() as service:
+        service.run()
